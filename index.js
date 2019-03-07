51f7a1c5440ad65a913320b60a3c4555d3a1ea43
Günther von Häfen <guenther.vonhaefen@gmx.de>
2019-03-07 17:02:19 +0100

    2.5 Edited Endpoints in index.js. Deleted /user/ in front of /movies. Also created clear different endpoints for requesting films by genre or director. Director Information is now inside the movies. Also edited the documentation.html that it fits to new index.js structure.

--- index.js
+++ index.js
@@ -20,7 +20,12 @@ let topMovies = [ {
   title : 'Snatch',
   description : 'Unscrupulous boxing promoters, violent bookmakers, a Russian gangster, incompetent amateur robbers and supposedly Jewish jewelers fight to track down a priceless stolen diamond.',
   genre : ['comedy', 'crime'],
-  director : 'Guy Ritchie',
+  director : {
+    name : 'Guy Ritchie',
+    bio : 'https://www.imdb.com/name/nm0005363/bio?ref_=nm_ov_bio_sm',
+    born : 1968,
+    died : 'n/a'
+  },
   imageURL : 'https://www.imdb.com/title/tt0208092/mediaviewer/rm1248859904'
 },
 {
@@ -28,7 +33,12 @@ let topMovies = [ {
   title : 'Lock, Stock & Two Smoking Barrels',
   description : 'A botched card game in London triggers four friends, thugs, weed-growers, hard gangsters, loan sharks and debt collectors to collide with each other in a series of unexpected events, all for the sake of weed, cash and two antique shotguns.',
   genre : ['comedy', 'crime'],
-  director : 'Guy Ritchie',
+  director : {
+    name : 'Guy Ritchie',
+    bio : 'https://www.imdb.com/name/nm0005363/bio?ref_=nm_ov_bio_sm',
+    born : 1968,
+    died : 'n/a'
+  },
   imageURL : 'https://www.imdb.com/title/tt0120735/mediaviewer/rm1138956032'
 },
 {
@@ -36,7 +46,12 @@ let topMovies = [ {
   title : 'MAD MAX: Fury Road',
   description : 'In a post-apocalyptic wasteland, a woman rebels against a tyrannical ruler in search for her homeland with the aid of a group of female prisoners, a psychotic worshiper, and a drifter named Max.',
   genre : ['action', 'adventure', 'sci-fi', 'thriller'],
-  director : 'George Miller',
+  director : {
+    name : 'George Miller',
+    bio : 'https://www.imdb.com/name/nm0004306/bio?ref_=nm_ov_bio_sm',
+    born : 1945,
+    died : 'n/a'
+  },
   imageURL : 'https://www.imdb.com/title/tt1392190/mediaviewer/rm3064749568'
 },
 {
@@ -44,7 +59,12 @@ let topMovies = [ {
   title : 'STAR WARS: The Last Jedi',
   description : 'Rey develops her newly discovered abilities with the guidance of Luke Skywalker, who is unsettled by the strength of her powers. Meanwhile, the Resistance prepares for battle with the First Order.',
   genre : ['action', 'adventure', 'sci-fi', 'fantasy'],
-  director : 'Rian Johnson',
+  director : {
+    name : 'Rian Johnson',
+    bio : 'https://www.imdb.com/name/nm0426059/bio?ref_=nm_ov_bio_sm',
+    born : 1973,
+    died : 'n/a'
+  },
   imageURL : 'https://www.imdb.com/title/tt2527336/mediaviewer/rm574104832'
 },
 {
@@ -52,7 +72,12 @@ let topMovies = [ {
   title : 'The Dark Knight',
   description : 'When the menace known as the Joker emerges from his mysterious past, he wreaks havoc and chaos on the people of Gotham. The Dark Knight must accept one of the greatest psychological and physical tests of his ability to fight injustice.',
   genre : ['action', 'crime', 'thriller', 'drama'],
-  director : 'Christopher Nolan',
+  director : {
+    name : 'Christopher Nolan',
+    bio : 'https://www.imdb.com/name/nm0634240/bio?ref_=nm_ov_bio_sm',
+    born : 1970,
+    died : 'n/a'
+  },
   imageURL : 'https://www.imdb.com/title/tt0468569/mediaviewer/rm4023877632'
 },
 {
@@ -60,7 +85,12 @@ let topMovies = [ {
   title : 'Captain America: Civil War',
   description : 'Political involvement in the Avengers affairs causes a rift between Captain America and Iron Man.',
   genre : ['action', 'adventure', 'sci-fi'],
-  director : 'Anthony Russo',
+  director : {
+    name : 'Anthony Russo',
+    bio : 'https://www.imdb.com/name/nm0751577/bio?ref_=nm_ov_bio_sm',
+    born : 1970,
+    died : 'n/a'
+  },
   imageURL : 'https://www.imdb.com/title/tt3498820/mediaviewer/rm3218348288'
 },
 {
@@ -68,7 +98,12 @@ let topMovies = [ {
   title : 'The Terminator',
   description : 'A seemingly indestructible android is sent from 2029 to 1984 to assassinate a waitress, whose unborn son will lead humanity in a war against the machines, while a soldier from that war is sent to protect her at all costs.',
   genre : ['action', 'sci-fi'],
-  director : 'James Cameron',
+  director : {
+    name : 'James Cameron',
+    bio : 'https://www.imdb.com/name/nm0000116/bio?ref_=nm_ov_bio_sm',
+    born : 1954,
+    died : 'n/a'
+  },
   imageURL : 'https://www.imdb.com/title/tt0088247/mediaviewer/rm774208512'
 },
 {
@@ -76,7 +111,12 @@ let topMovies = [ {
   title : 'Casino Royale',
   description : 'Armed with a license to kill, Secret Agent James Bond sets out on his first mission as 007, and must defeat a private banker to terrorists in a high stakes game of poker at Casino Royale, Montenegro, but things are not what they seem.',
   genre : ['action', 'adventure', 'thriller'],
-  director : 'Martin Campbell',
+  director : {
+    name : 'Martin Campbell',
+    bio : 'https://www.imdb.com/name/nm0132709/bio?ref_=nm_ov_bio_sm',
+    born : 1943,
+    died : 'n/a'
+  },
   imageURL : 'https://www.imdb.com/title/tt0381061/mediaviewer/rm3667992064'
 },
 {
@@ -84,7 +124,12 @@ let topMovies = [ {
   title : 'The Lord of the Rings: The Two Towers',
   description : 'While Frodo and Sam edge closer to Mordor with the help of the shifty Gollum, the divided fellowship makes a stand against Saurons new ally, Saruman, and his hordes of Isengard.',
   genre: ['drama', 'adventure', 'fantasy'],
-  director : 'Peter Jackson',
+  director : {
+    name : 'Peter Jackson',
+    bio : 'https://www.imdb.com/name/nm0001392/bio?ref_=nm_ov_bio_sm',
+    born : 1961,
+    died : 'n/a'
+  },
   imageURL : 'https://www.imdb.com/title/tt0167261/mediaviewer/rm2020826368'
 },
 {
@@ -92,77 +137,14 @@ let topMovies = [ {
   title : 'Black Panther',
   description : 'TChalla, heir to the hidden but advanced kingdom of Wakanda, must step forward to lead his people into a new future and must confront a challenger from his countrys past.',
   genre : ['action', 'adventure', 'sci-fi'],
-  director : 'Ryan Coogler',
+  director : {
+    name : 'Ryan Coogler',
+    bio : 'https://www.imdb.com/name/nm3363032/bio?ref_=nm_ov_bio_sm',
+    born : 1986,
+    died : 'n/a'
+  },
   imageURL : 'https://www.imdb.com/title/tt1825683/mediaviewer/rm172972800'
-}
-]
-
-// directors list
-let directors = [{
-  id : '1',
-  name : 'Peter Jackson',
-  bio : 'https://www.imdb.com/name/nm0001392/bio?ref_=nm_ov_bio_sm',
-  born : 1961,
-  died : 'n/a'
-},
-{
-  id : '2',
-  name : 'Ryan Coogler',
-  bio : 'https://www.imdb.com/name/nm3363032/bio?ref_=nm_ov_bio_sm',
-  born : 1986,
-  died : 'n/a'
-},
-{
-  id : '3',
-  name : 'Martin Campbell',
-  bio : 'https://www.imdb.com/name/nm0132709/bio?ref_=nm_ov_bio_sm',
-  born : 1943,
-  died : 'n/a'
-},
-{
-  id : '4',
-  name : 'James Cameron',
-  bio : 'https://www.imdb.com/name/nm0000116/bio?ref_=nm_ov_bio_sm',
-  born : 1954,
-  died : 'n/a'
-},
-{
-  id : '5',
-  name : 'Anthony Russo',
-  bio : 'https://www.imdb.com/name/nm0751577/bio?ref_=nm_ov_bio_sm',
-  born : 1970,
-  died : 'n/a'
-},
-{
-  id : '6',
-  name : 'Christopher Nolan',
-  bio : 'https://www.imdb.com/name/nm0634240/bio?ref_=nm_ov_bio_sm',
-  born : 1970,
-  died : 'n/a'
-},
-{
-  id : '7',
-  name : 'Rian Johnson',
-  bio : 'https://www.imdb.com/name/nm0426059/bio?ref_=nm_ov_bio_sm',
-  born : 1973,
-  died : 'n/a'
-},
-{
-  id : '8',
-  name : 'George Miller',
-  bio : 'https://www.imdb.com/name/nm0004306/bio?ref_=nm_ov_bio_sm',
-  born : 1945,
-  died : 'n/a'
-},
-{
-  id : '9',
-  name : 'Guy Ritchie',
-  bio : 'https://www.imdb.com/name/nm0005363/bio?ref_=nm_ov_bio_sm',
-  born : 1968,
-  died : 'n/a'
-}
-
-];
+}];
 
 // users list
 let users = [{
@@ -198,42 +180,45 @@ app.use(function (err, req, res, next) {
 /////////////
 
 // returns json object of topMovies object to user
-app.get('/user/movies', function(req, res) {
+app.get('/movies', function(req, res) {
   res.json(topMovies);
 });
 
-// returns JSON object of single movie by title to user
-app.get('/user/movies/:title/:genre', (req, res) => {
-  console.log( require('util').inspect( req.params) );    // for testing purposes!!! NEEDS TO BE DELETED IN FINAL VERSION
-  let title = topMovies.find((movie) => {
+// returns JSON object of single movie by title
+app.get('/movies/:title', (req, res) => {
+  res.json(topMovies.find((movie) => {
     return movie.title === req.params.title
-  });
-  let genre = topMovies.filter(function(obj) {
-    return obj.genre.includes(req.params.genre.toLowerCase())
-  });
-
-  if (title) {
-    res.json(title);
-  } else if (genre) {
-    res.json(genre);
-  } else {
-    res.status(404).send('No film matching your request was not found.')
-  }
-});
-
-/*app.get("/user/movies/:title/:genre/:director", (req, res) => {
- res.send('Successful GET request returning data of single movie/ list of movies by genre / details on director');
-});*/
-
-// returns JSON object about director by name to user
-app.get('/directors/:name', function(req, res) {
-  res.json(directors.find((director) => {
-    return director.name === req.params.name
   }));
 });
 
+// returns JSON Object containing every movie matching the genre
+app.get('/movies/genres/:genre', (req, res) => {
+  res.json(topMovies.filter((movie) => {
+    return movie.genre.includes(req.params.genre.toLowerCase())
+  }));
+});
+
+// returns JSON Object containing every movie matching the dirrector
+app.get('/movies/directors/:director', (req, res) => {
+  res.json(topMovies.filter((movie) => {
+    return movie.director.name === req.params.director
+  }));
+});
+
+// returns JSON object with detailed director information by director name to user
+app.get('/directors/:name', function(req, res) {
+  let findDirector = topMovies.find((movie) => {
+    return movie.director.name === req.params.name
+  });
+
+  if (findDirector) {
+    directorDetails = findDirector.director;
+    res.json(directorDetails);
+  }
+});
+
 // adds a new movie to list
-app.post('/user/movies', (req, res) => {
+app.post('/movies', (req, res) => {
   let newMovie = req.body;
 
   if (!newMovie.title) {
@@ -247,7 +232,7 @@ app.post('/user/movies', (req, res) => {
 });
 
 // deletes a movie from the topMovies list by name
-app.delete('/user/movies/:id', (req, res) => {
+app.delete('/movies/:id', (req, res) => {
   let movie = topMovies.find((obj) =>
     { return obj.id === req.params.id });
 
@@ -262,7 +247,7 @@ app.delete('/user/movies/:id', (req, res) => {
 /////////////
 
 // create new user
-app.post('/user', (req, res) => {
+app.post('/users', (req, res) => {
   let newUser = req.body;
 
   if (!newUser.username) {
@@ -276,7 +261,7 @@ app.post('/user', (req, res) => {
 });
 
 // deletes user from users by ID
-app.delete('/user/:id', (req, res) => {
+app.delete('/users/:id', (req, res) => {
   let user = users.find((obj) =>
     { return obj.id === req.params.id });
 
@@ -287,7 +272,7 @@ app.delete('/user/:id', (req, res) => {
 });
 
 // update the user info (username, password, email, date of birth) by ID (is the only unchangable identification)
-app.put('/user/:id/:username/:password/:email/:dateOfBirth', (req, res) => {
+app.put('/users/:id/:username/:password/:email/:dateOfBirth', (req, res) => {
   let user = users.find( (user) => {
     return user.id === req.params.id
   });
