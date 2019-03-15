ba01d4386fb7d018526834c74d9a331a9f7cfb39
Günther von Häfen <>
2019-03-15 05:52:28 +0100

    2.9 Created passport.js file to define Local and JWT Strategies.

---
+++ passport.js
@@ -0,0 +1,51 @@
+////////////////
+// IMPORT RELEVANT MODULES
+////////////////
+
+const passport = require('passport'),
+      LocalStrategy = require('passport-local').Strategy,
+      Models = require('./models.js'),
+      passportJWT = require('passport-jwt');
+
+var Users = Models.User;
+var JWTStrategy = passportJWT.Strategy;
+var ExtractJWT = passportJWT.ExtractJwt;
+
+////////////////
+// DEFINE PASSPORT STRATEGIES
+////////////////
+
+// LocalStrategy to define basic HTTP authentication for login requests
+passport.use(new LocalStrategy({
+  usernameField: 'Username',
+  passwordField: 'Password'
+}, (username, password, callback) => {
+  ;                                                 //WHAT IS WRONG HERE??????
+  console.log(username + '  ' + password);
+  Users.findOne({Username: username, Password: password}, (error, user) => {
+    if (error) {
+      console.log(error);
+      return callback(error);
+    }
+    if (!user) {
+      console.log('incorrect username or password');
+      return callback(null, false, {message: 'Incorrect username'});
+    }
+    console.log('finished');
+    return callback(null, user);
+  });
+}));
+
+// JWT Strategy to allow to authenticate users based on the JWT
+passport.use(new JWTStrategy({
+  jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+  secretOrKey: 'your_jwt_secret'
+}, (jwtPayload, callback) => {
+  return Users.findById(jwtPayload._id)
+  .then((user) => {
+    return callback(null, user);
+  })
+  .catch((error) => {
+    return callback(error)
+  });
+}));
