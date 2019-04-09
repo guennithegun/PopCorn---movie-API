8ed1cfeb5f2634890016e06cba87088cb3ae7e4e
Günther von Häfen <>
2019-04-09 02:30:20 +0200

    update

--- dist/src.78399e21.js
+++ dist/src.78399e21.js
@@ -27919,7 +27919,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52053" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59563" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
