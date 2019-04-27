96b9df165901e40679f67c91ea5b0b717a67fbce
Günther von Häfen <guenther.vonhaefen@gmx.de>
2019-04-28 01:46:40 +0200

    3.4: Added login-view.css file for applying component styles. Also changed handleSubmit function to create User for testing purposes.

--- client/src/components/login-view/login-view.jsx
+++ client/src/components/login-view/login-view.jsx
@@ -3,6 +3,9 @@
 ////////////
 import React, { useState } from 'react';
 import axios from 'axios';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import './login-view.scss';
 
 ////////////
 // DECLARE AND EXPORT FUNCTION COMPONENTS
@@ -14,27 +17,39 @@ export function LoginView(props) {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    axios.post('https://popcorn-movieapp.herokuapp.com/login', {})
-    .then(response => {
-      console.log(response);
-    })
-    .catch(error => {
-      console.log(error);
-    });
-    //console.log(username, password);
+    console.log(username, password);
+    props.onLoggedIn(username);
   };
 
   return (
-    <form>
-      <label>
-        Username:
-        <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
-      </label>
-      <label>
-        Password:
-        <input type="text" value={password} onChange={e => setPassword(e.target.value)} />
-      </label>
-      <button type="button" onClick={handleSubmit}>Submit</button>
-    </form>
+    // <form>
+    //   <label>
+    //     Username:
+    //     <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+    //   </label>
+    //   <label>
+    //     Password:
+    //     <input type="text" value={password} onChange={e => setPassword(e.target.value)} />
+    //   </label>
+    //   <button type="button" onClick={handleSubmit}>Submit</button>
+    // </form>
+    <Form>
+      <Form.Group controlId="formBasicEmail">
+        <Form.Label>Username</Form.Label>
+        <Form.Control type="email" placeholder="Enter Username" />
+        <Form.Text className="text-muted">
+        Type your username here.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+
+      <Button variant="primary" type="submit">
+      Submit
+      </Button>
+    </Form>
   );
 }
