8b08fc44ebdf2f565c714101b05ca0dc50f1eaa4
Günther von Häfen <>
2019-04-28 04:30:34 +0200

    3.4: Implemented bootstrap cards.

--- client/src/components/movie-card/movie-card.jsx
+++ client/src/components/movie-card/movie-card.jsx
@@ -3,6 +3,8 @@
 ////////////
 import React from 'react';
 import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 ////////////
 // DECLARE AND EXPORT COMPONENTS
@@ -12,7 +14,15 @@ export class MovieCard extends React.Component {
     const { movie, onClick } = this.props;
 
     return (
-      <div className="movie-card" onClick={() => onClick(movie)}>{movie.Title}</div>
+      //<div className="movie-card" onClick={() => onClick(movie)}>{movie.Title}</div>
+      <Card style={{ width: '16rem' }} xs={4}>
+        <Card.Img variant="top" src={movie.ImagePath} />
+        <Card.Body>
+          <Card.Title>{movie.Title}</Card.Title>
+          <Card.Text>{movie.Description}</Card.Text>
+          <Button onClick={() => onClick(movie)} variant="link">Open</Button>
+        </Card.Body>
+      </Card>
     );
   }
 }
