1bb4686ba542f6e49fe51a252825d413ba42201b
Günther von Häfen <guenther.vonhaefen@gmx.de>
2019-04-30 02:44:31 +0200

    3.4: Implemented bootstraps <Container>, <Row> and <Col> tags for using the card component.

--- client/src/components/main-view/main-view.jsx
+++ client/src/components/main-view/main-view.jsx
@@ -3,6 +3,9 @@
 ////////////
 import React from 'react';
 import axios from 'axios';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
 
 import { LoginView } from '../login-view/login-view';
 import { MovieCard } from '../movie-card/movie-card';
@@ -59,12 +62,18 @@ export class MainView extends React.Component {
     if (!movies) return <div className="main-view"/>;
     return (
       <div className="main-view">
+      <Container>
+      <Row>
       {selectedMovie
         ? <MovieView movie={selectedMovie} onClick={() => this.getBackClick()}/>
         : movies.map(movie => (
+          <Col key={movie._id} xs={12} sm={6} md={4}>
           <MovieCard key={movie._id} movie={movie} onClick={movie => this.onMovieClick(movie)}/>
+          </Col>
         ))
       }
+      </Row>
+      </Container>
       </div>
     );
   }
