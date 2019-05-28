94daf15b616f01ae78438eb0d379e653e8e50867
Günther von Häfen <guenther.vonhaefen@gmx.de>
2019-05-28 11:47:44 +0200

    3.6: Created Visibility-Filter-Input component.

---
+++ client/src/components/visibility-filter-input/visibility-filter-input.jsx
@@ -0,0 +1,23 @@
+////////////
+// IMPORT ALL NECESSARY MODULES AND FILES
+////////////
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+import Form from 'react-bootstrap/Form';
+
+import { setFilter } from '../../actions/actions';
+
+function VisibilityFilterInput(props) {
+  return <Form.Control
+    onChange={event => props.setFilter(event.target.value)}
+    value={props.visibilityFilter}
+    placeholder='filter'
+  />;
+}
+
+export default connect(
+  ({visibilityFilter}) => ({visibilityFilter}),
+  { setFilter }
+)(VisibilityFilterInput);
