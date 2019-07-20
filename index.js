870c1fd038e2ba535330a9635c96f9f78b38bb35
Günther von Häfen <>
2019-07-20 21:41:44 +0200

    3.9#######. Changes for Heroku deploy.

--- index.js
+++ index.js
@@ -334,6 +334,15 @@ app.delete('/users/:username/movies/:movieid', passport.authenticate('jwt', { se
   })
 });
 
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
 // default textual response when request hits the root folder
 app.get('/', function(req, res) {
   res.send('Welcome to PopCorn. The best movie App ever :-P');
